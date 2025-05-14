4a0599d8beb24365b267399e1460bbd3bdc60f8e
pavankumar2004 <pk6122004@gmail.com>
2025-05-14 23:31:07 +0530

    firebase config

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyA4sFvl7_P-NOqMb_dAkqJOCagVXNsaiKA",
+  authDomain: "airdropinder.firebaseapp.com",
+  projectId: "airdropinder",
+  storageBucket: "airdropinder.appspot.com", // Fixed storage bucket format
+  messagingSenderId: "777680799742",
+  appId: "1:777680799742:web:649b2f77046b73595aa2af",
+  measurementId: "G-TWPGV33WN8"
 };
 
 // Initialize Firebase
